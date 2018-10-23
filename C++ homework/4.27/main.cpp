5d1144904c2d6bb58c17fadccd1ab8d4ee6c6127
DESKTOP-07K288C\songyhenglian <2803102942@qq.com>
2018-10-23 21:54:35 +0800

    no message

---
+++ C++ homework/4.27/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a=0,b=0,c=0,s=0;
+   b=(a%10)*1;
+   c=[a-a%10]/5;
+   s=b+c;
+
+   cout<<"Enter the first number\n"<<endl;
+   cin>>a;
+
+   cout<<"b is"<<b<<endl;
+   cout<<"c is"<<c<<endl;
+   cout<<"s is"<<s<<endl;
+
+
+}
